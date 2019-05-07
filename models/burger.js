dbadb65ba60ff17fb66cd5ff0bca8f668bc3bf2b
Serena Stephens <stephensserena@gmail.com>
2019-05-07 13:29:05 -0700

    Application is now Fully Functional and Finalized final CSS touches

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,24 @@
-// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
-    orm.selectAll("burgers", function(res) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
-  create: function(vals, cb) {
-    orm.insertOne("burgers", ['burger_name'], vals, function(res) {
-        cb(res);
-    });
-},
-  update: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
+  create: function(name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+      name, false
+    ], cb);
   },
+  update: function(id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
+  }
 };
 
-// Export the database functions for the controller (catsController.js).
 module.exports = burger;
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,60 +1,17 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    $(".change-devour").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevour = $(this).data("newdevour");
-  
-      var newDevourState = {
-        devour: newDevour
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevourState
-      }).then(
-        function() {
-          console.log("changed devour to", newDevour);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+$(document).ready(function() {
+    
+  $(".devour-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var burger_id = $(this).children(".burger_id").val();
+    console.log(burger_id);
+    $.ajax({
+      method: "PUT",
+      url: "/burgers/" + burger_id
+    }).then(function(data) {
+      // reload page to display devoured burger in proper column
+      location.reload();
     });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#ca").val().trim(),
-        devour: $("[burger_name=devour]:checked").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".delete-burger").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/burgers/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted burger", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+
   });
+});
